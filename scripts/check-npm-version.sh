4917c345eacbb4ac22a9995a0b586567d090f468
Sonmez Kartal <sonmez@koding.com>
2016-01-06 20:43:29 +0200

    Update npm version requirement to 3.x

--- scripts/check-npm-version.sh
+++ scripts/check-npm-version.sh
@@ -3,14 +3,12 @@
 VERSION=$(npm --version)
 
 while IFS=".", read MAJOR MINOR REVISION; do
-  if [[ $MAJOR -ne 2 ]]; then
-    MISMATCH=1
-  elif [[ $MAJOR -eq 2 && $MINOR -lt 15 ]]; then
+  if [[ $MAJOR -lt 3 ]]; then
     MISMATCH=1
   fi
 done < <(echo $VERSION)
 
 if [[ -n "$MISMATCH" ]]; then
-  echo "error: npm version must be 2.15.x or greater"
+  echo "error: npm version must be 3.x or greater"
   exit 1
 fi
